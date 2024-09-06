3448953dc1a46274cc67d2148efb82d678f1fbb6
rkr <ron.kirschler@gmail.com>
2024-09-06 12:10:00 +0200

    AFDeliveryTokenProvider: Cache the token

--- src/AFDelivery/AFDeliveryTokenProvider.php
+++ src/AFDelivery/AFDeliveryTokenProvider.php
@@ -2,11 +2,16 @@
 
 namespace EcommerceUtilities\DHL\AFDelivery;
 
+use DateTimeImmutable;
+use DateTimeInterface;
 use EcommerceUtilities\DHL\AFDelivery\TokenProvider\NoTokenException;
 use EcommerceUtilities\DHL\Common\DHLTools;
 use JsonException;
 
 class AFDeliveryTokenProvider {
+	/** @var null|array{token: string, expiresAt: DateTimeInterface} */
+	private null|array $cache = null;
+
 	public function __construct(
 		private readonly AFDeliveryHttpClient $client,
 		private readonly string $username,
@@ -14,23 +19,32 @@ class AFDeliveryTokenProvider {
 	) {}
 
 	public function getToken(): string {
-		$base64 = base64_encode(sprintf('%s:%s', $this->username, $this->password));
-		$response = $this->client->get('login/3.0.0', [
-			'headers' => [
-				'Authorization' => sprintf('Basic %s', $base64)
-			]
-		]);
+		if($this->cache === null || $this->cache['expiresAt'] < new DateTimeImmutable()) {
+			$base64 = base64_encode(sprintf('%s:%s', $this->username, $this->password));
+			$response = $this->client->get('login/3.0.0', [
+				'headers' => [
+					'Authorization' => sprintf('Basic %s', $base64)
+				]
+			]);
 
-		try {
-			$data = DHLTools::jsonDecode($response->body);
+			try {
+				$data = DHLTools::jsonDecode($response->body);
 
-			if(($data['access_token'] ?? null) === null) {
+				if(($data['access_token'] ?? null) === null) {
+					throw new NoTokenException('No access token found in response');
+				}
+
+				$this->cache = [
+					'token' => $data['access_token'],
+					'expiresAt' => (new DateTimeImmutable())
+						->setTimestamp($data['expires_in_epoch_seconds'])
+						->modify('-1 minute')
+				];
+			} catch(JsonException) {
 				throw new NoTokenException('No access token found in response');
 			}
-
-			return $data['access_token'];
-		} catch(JsonException) {
-			throw new NoTokenException('No access token found in response');
 		}
+
+		return $this->cache['token'];
 	}
 }
