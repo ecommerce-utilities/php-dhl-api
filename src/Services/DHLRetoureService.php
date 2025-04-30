781cf361fed3359ff96aa007b248dc17ab900e73
rkr <ron.kirschler@gmail.com>
2025-04-30 14:12:10 +0200

    Refactor DHLRetoureService: Simplify request handling and use HttpClient

--- src/Services/DHLRetoureService.php
+++ src/Services/DHLRetoureService.php
@@ -5,6 +5,7 @@ use EcommerceUtilities\DHL\Common\DHLApiCredentials;
 use EcommerceUtilities\DHL\Common\DHLApiException;
 use EcommerceUtilities\DHL\Common\DHLBusinessPortalCredentials;
 use EcommerceUtilities\DHL\Common\DHLTools;
+use EcommerceUtilities\DHL\Http\HttpClient;
 use EcommerceUtilities\DHL\Services\DHLRetoureService\DHLRetoureServiceResponse;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
@@ -15,29 +16,16 @@ class DHLRetoureService {
 	public function __construct(
 		private readonly DHLBusinessPortalCredentials $businessPortalCredentials,
 		private readonly DHLApiCredentials $credentials,
-		private readonly RequestFactoryInterface $requestFactory,
-		private readonly StreamFactoryInterface $streamFactory,
-		private readonly ClientInterface $client
+		private readonly HttpClient $client
 	) {}
 
 	/**
 	 * @return DHLRetoureServiceResponse
 	 */
 	public function getRetourePdf(string $name1, ?string $name2, ?string $name3, string $street, string $streetNumber, string $zip, string $city, string $countryId, ?string $voucherNr = null, ?string $shipmentReference = null): DHLRetoureServiceResponse {
-		$request = $this->requestFactory->createRequest('POST', $this->credentials->isProductionEnv() ? 'https://cig.dhl.de/services/production/rest/returns/' : 'https://cig.dhl.de/services/sandbox/rest/returns/');
+		$uri = $this->credentials->isProductionEnv() ? 'https://cig.dhl.de/services/production/rest/returns/' : 'https://cig.dhl.de/services/sandbox/rest/returns/';
 
-		$auth = static function ($username, $password) {
-			$basicAuthCredentials = sprintf('%s:%s', $username, $password);
-			return base64_encode($basicAuthCredentials);
-		};
-
-		$request = $request
-			->withHeader('Authorization', sprintf('Basic %s', $auth($this->credentials->getUsername(), $this->credentials->getPassword())))
-			->withHeader('DPDHL-User-Authentication-Token', $auth($this->businessPortalCredentials->getUsername(), $this->businessPortalCredentials->getPassword()))
-			->withHeader('Accept', 'application/json')
-			->withHeader('Content-Type', 'application/json');
-
-		$data = DHLTools::jsonEncode([
+		$body = DHLTools::jsonEncode([
 			'receiverId' => $this->credentials->getReceiverId(),
 			'customerReference' => $voucherNr,
 			'shipmentReference' => $shipmentReference,
@@ -55,13 +43,19 @@ class DHLRetoureService {
 			'returnDocumentType' => 'SHIPMENT_LABEL'
 		]);
 
-		$request = $request->withBody($this->streamFactory->createStream($data));
-		$response = $this->client->sendRequest($request);
+		$auth = static fn($username, $password) => base64_encode(sprintf('%s:%s', $username, $password));
 
-		$result = $response->getBody()->getContents();
+		$headers = [
+			'Authorization' => sprintf('Basic %s', $auth($this->credentials->getUsername(), $this->credentials->getPassword())),
+			'DPDHL-User-Authentication-Token' => $auth($this->businessPortalCredentials->getUsername(), $this->businessPortalCredentials->getPassword()),
+			'Accept' => 'application/json',
+			'Content-Type' => 'application/json',
+		];
+
+		$response = $this->client->post($uri, $body, ['headers' => $headers]);
 
 		try {
-			$data = DHLTools::jsonDecode($result, asObject: true);
+			$data = DHLTools::jsonDecode($response->body, asObject: true);
 
 			if(($data->code ?? 0) > 0) {
 				throw new DHLApiException(sprintf("DHL-Error: %s (%d)", $data->detail ?? 'No details provided', $data->code ?? 0));
